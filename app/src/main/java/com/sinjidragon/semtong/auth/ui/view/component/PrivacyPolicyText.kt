8e07bf1aea536e32b3d87495ddd5fce55fdb960c
iszero12 <binj1246@gmail.com>
2024-10-21 22:09:20 +0900

    feat :: set web page access

--- app/src/main/java/com/sinjidragon/semtong/auth/ui/view/component/PrivacyPolicyText.kt
+++ app/src/main/java/com/sinjidragon/semtong/auth/ui/view/component/PrivacyPolicyText.kt
@@ -1,5 +1,7 @@
 package com.sinjidragon.semtong.auth.ui.view.component
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -15,26 +18,28 @@ import com.sinjidragon.semtong.ui.theme.pretendard
 import com.sinjidragon.semtong.ui.theme.subColor
 
 @Composable
-fun PrivacyPolicyText(modifier: Modifier){
+fun PrivacyPolicyText(modifier: Modifier) {
+    val context = LocalContext.current
+
     Column(
         modifier = modifier
-    ){
+    ) {
         Text(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
             text = "계정을 생성함으로써,",
             fontFamily = pretendard,
             fontWeight = FontWeight.Medium,
             fontSize = 12.sp,
             color = gray2
         )
-        Row (
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally),
-        ){
+        Row(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+        ) {
             Text(
-                modifier = Modifier
-                    .clickable{ TODO()},
+                modifier = Modifier.clickable {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://semtong1.notion.site/1265ad66b02780c3bebee12c3c82ce60?pvs=4"))
+                    context.startActivity(intent)
+                },
                 text = "이용약관",
                 fontFamily = pretendard,
                 fontWeight = FontWeight.Medium,
@@ -49,8 +54,10 @@ fun PrivacyPolicyText(modifier: Modifier){
                 color = gray2
             )
             Text(
-                modifier = Modifier
-                    .clickable{ TODO()},
+                modifier = Modifier.clickable {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://semtong1.notion.site/1265ad66b02780cc9f9dc41dd8d4f974?pvs=4"))
+                    context.startActivity(intent)
+                },
                 text = "개인정보처리방침",
                 fontFamily = pretendard,
                 fontWeight = FontWeight.Medium,
@@ -67,11 +74,12 @@ fun PrivacyPolicyText(modifier: Modifier){
         }
     }
 }
+
 @Preview(
     showBackground = true,
     showSystemUi = true
 )
 @Composable
-fun PrivacyPolicyTextPreview(){
+fun PrivacyPolicyTextPreview() {
     PrivacyPolicyText(modifier = Modifier)
 }
