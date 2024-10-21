055d637ec2b2421e3896d8580a69cef8d6345477
iszero12 <binj1246@gmail.com>
2024-10-21 19:25:13 +0900

    feat :: Add NavGroup

---
+++ app/src/main/java/com/sinjidragon/semtong/nav/NavGroup.kt
@@ -0,0 +1,8 @@
+package com.sinjidragon.semtong.nav
+
+object NavGroup {
+    const val INTRO = "intro"
+    const val LOGIN = "login"
+    const val SIGNUP1 = "signup1"
+    const val SIGNUP2 = "signup2"
+}
