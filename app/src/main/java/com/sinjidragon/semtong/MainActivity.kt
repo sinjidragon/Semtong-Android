ae74860d35d7d94c72d43561cfc894ac4d97995f
iszero12 <binj1246@gmail.com>
2024-10-22 20:28:18 +0900

    fix :: add MainActivity

--- app/src/main/java/com/sinjidragon/semtong/MainActivity.kt
+++ app/src/main/java/com/sinjidragon/semtong/MainActivity.kt
@@ -4,44 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.sinjidragon.semtong.ui.theme.SemtongTheme
+import androidx.navigation.compose.rememberNavController
+import com.sinjidragon.semtong.nav.NavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SemtongTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            val navHostController = rememberNavController()
+            NavGraph(navController = navHostController)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SemtongTheme {
-        Greeting("Android")
     }
 }
