4fd2f806511c33258c210cbb9255f3908d5c12b7
iszero12 <binj1246@gmail.com>
2024-10-22 16:56:53 +0900

    delete :: currentRoute

--- app/src/main/java/com/sinjidragon/semtong/nav/currentRoute.kt
+++
@@ -1,7 +0,0 @@
-package com.sinjidragon.semtong.nav
-
-import androidx.navigation.NavController
-
-fun currentRoute(navController: NavController): String?{
-    return navController.currentBackStackEntry?.destination?.route
-}
