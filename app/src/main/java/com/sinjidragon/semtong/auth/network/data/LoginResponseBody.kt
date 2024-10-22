b51ac754fad9ebf5aaa2da32c6ec3e7c38e53282
iszero12 <binj1246@gmail.com>
2024-10-22 19:54:10 +0900

    fix :: add userRole

--- app/src/main/java/com/sinjidragon/semtong/auth/network/data/LoginResponseBody.kt
+++ app/src/main/java/com/sinjidragon/semtong/auth/network/data/LoginResponseBody.kt
@@ -1,6 +1,7 @@
 package com.sinjidragon.semtong.auth.network.data
 
 data class LoginResponseBody (
+    val userRole : String,
     val accessToken : String,
     val refreshToken : String,
     val tokenType : String
