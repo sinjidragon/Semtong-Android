8b40feb1a6a0969a3814d740656138f04f0a6ce1
iszero12 <binj1246@gmail.com>
2024-10-22 23:23:15 +0900

    fix :: AuthService

--- app/src/main/java/com/sinjidragon/semtong/auth/network/AuthService.kt
+++ app/src/main/java/com/sinjidragon/semtong/auth/network/AuthService.kt
@@ -23,5 +23,4 @@ interface AuthService {
     suspend fun refresh(@Body request: RefreshRequestBody): LoginResponseBody
     @POST("/auth/signup")
     suspend fun signup(@Body request: SignupRequestBody)
-
 }
