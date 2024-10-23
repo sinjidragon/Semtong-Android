46dcd6c9a113fe4426d9d1a761f836b3c2be4267
iszero12 <binj1246@gmail.com>
2024-10-23 21:20:39 +0900

    fix :: del Log

--- app/src/main/java/com/sinjidragon/semtong/auth/network/api/login.kt
+++ app/src/main/java/com/sinjidragon/semtong/auth/network/api/login.kt
@@ -1,10 +1,8 @@
 package com.sinjidragon.semtong.auth.network.api
 
 import android.content.Context
-import android.util.Log
 import com.sinjidragon.semtong.auth.network.RetrofitClient
 import com.sinjidragon.semtong.auth.network.data.LoginRequestBody
-import com.sinjidragon.semtong.auth.network.data.LoginResponseBody
 import com.sinjidragon.semtong.auth.network.parseErrorResponse
 import com.sinjidragon.semtong.auth.network.token.saveAccToken
 import com.sinjidragon.semtong.auth.network.token.saveRefToken
@@ -22,9 +20,7 @@ suspend fun login(context: Context, username: String, password: String): String 
             saveRefToken(context, response.refreshToken)
             "success"
         } catch (e: HttpException) {
-            Log.d("login", "HttpException: ${e.response()?.toString()}")
             val errorBody = e.response()?.errorBody()?.string()
-            Log.d("login", "Error body: $errorBody")
             val errorResponse = errorBody?.let { parseErrorResponse(it) }
             errorResponse?.error ?: "알 수 없는 오류가 발생했습니다."
         } catch (e: Exception) {
