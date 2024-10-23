10c2714cbc804a9a5a8969f4dbeb6f221ff50b55
iszero12 <binj1246@gmail.com>
2024-10-23 21:21:16 +0900

    fix :: fix saveToken

--- app/src/main/java/com/sinjidragon/semtong/auth/network/token/saveToken.kt
+++ app/src/main/java/com/sinjidragon/semtong/auth/network/token/saveToken.kt
@@ -5,8 +5,8 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.runBlocking
 
-val REF_TOKEN = stringPreferencesKey("auth_token")
-val ACC_TOKEN = stringPreferencesKey("auth_token")
+val REF_TOKEN = stringPreferencesKey("ref_token")
+val ACC_TOKEN = stringPreferencesKey("acc_token")
 val ROLES = stringPreferencesKey("roles")
 
 fun saveRefToken(context: Context, token: String) {
