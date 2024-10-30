ded1e0089d77d77e82f595f8aa2f2d113a45dfe2
iszero12 <binj1246@gmail.com>
2024-10-30 10:45:09 +0900

    fix :: Set NavBar, AppBar

--- app/src/main/java/com/sinjidragon/semtong/main/ui/screen/GoalView.kt
+++ app/src/main/java/com/sinjidragon/semtong/main/ui/screen/GoalView.kt
@@ -1,8 +1,25 @@
 package com.sinjidragon.semtong.main.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
+import com.sinjidragon.semtong.main.ui.component.AppBar
+import com.sinjidragon.semtong.main.ui.component.BottomNav
 
 @Composable
 fun GoalView (navController : NavController){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .systemBarsPadding()
+    ){
+        AppBar()
+        BottomNav(navController = navController)
+    }
 }
--- app/src/main/java/com/sinjidragon/semtong/main/ui/screen/HomeView.kt
+++ app/src/main/java/com/sinjidragon/semtong/main/ui/screen/HomeView.kt
@@ -1,8 +1,35 @@
 package com.sinjidragon.semtong.main.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.sinjidragon.semtong.main.ui.component.AppBar
+import com.sinjidragon.semtong.main.ui.component.BottomNav
 
 @Composable
 fun HomeView (navController : NavController ){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .systemBarsPadding()
+    ){
+        AppBar()
+        BottomNav(navController = navController)
+    }
+}
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun HomeViewPreview(){
+    HomeView(navController = NavController(context = LocalContext.current))
 }
